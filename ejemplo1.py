b887659c2da1a06109a59cb85e8cdbbb42d1bc4f
a01745043 <bruno_estrada@DESKTOP-N1JJTVP.localdomain>
2020-10-29 13:04:19 -0600

    imagen escala de grises

--- ejemplo1.py
+++ ejemplo1.py
@@ -8,5 +8,11 @@ print(imagen[0][0][0])
 imagen = cv2.resize(imagen,(256,256))
 
 cv2.imwrite('resizeimagen.jpg',imagen)
-cv2.imshow('image',imagen)
+#cv2.imshow('image',imagen)
+
+imagen = cv2.imread('imagen.jpg')
+imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+print(imagen.shape)
+print(imagen[0][0])
+cv2.imwrite('grayimagen.jpg',imagen)
 
